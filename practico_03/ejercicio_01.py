638860c0ab62ff1275e409f770c4b16b1d6f37cb
MarcosLeones <87377508+MarcosLeones@users.noreply.github.com>
2022-04-12 15:31:44 -0300

    Update ejercicio_01.py

--- practico_03/ejercicio_01.py
+++ practico_03/ejercicio_01.py
@@ -8,7 +8,12 @@ class Rectangulo:
     Implementar la clase Rectangulo que contiene una base y una altura, y el
     método area.
     """
-    # Completar
+    def __init__(self, base=None, altura=None):
+        self.base=base
+        self.altura=altura
+
+    def area(self):
+        return (self.base*self.altura) if (self.base!=None and self.altura!=None) else 0
 
 
 # NO MODIFICAR - INICIO
