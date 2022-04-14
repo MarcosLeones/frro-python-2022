f819ef8da29ff3702f6b0b8c7f58c37da72a8e86
MarcosLeones <87377508+MarcosLeones@users.noreply.github.com>
2022-04-14 20:10:58 -0300

    Update ejercicio_12.py

--- practico_01/ejercicio_12.py
+++ practico_01/ejercicio_12.py
@@ -19,7 +19,12 @@ def combinar_basico(nombres: List[str], precios: List[float]) -> Tuple[Any]:
 
     Restricción: Resolver utilizando un bucle for.
     """
-    pass # Completar
+    t=tuple()
+    for i in range (len(nombres)):
+        dupla = (nombres[i],precios[i])
+        tupla = (dupla,)
+        t = t+tupla
+    return t
 
 
 # NO MODIFICAR - INICIO
@@ -67,7 +72,7 @@ def combinar_zip(nombres: List[str], precios: List[float], ids: List[int]) -> Tu
     """Re-Escribir utilizando zip.
     Referencia: https://docs.python.org/3/library/functions.html#zip
     """
-    pass # Completar
+    return tuple(zip(nombres,precios,ids))
 
 
 # NO MODIFICAR - INICIO
@@ -93,7 +98,7 @@ def combinar_zip_args(*args) -> Tuple[Any]:
     """Re-Escribir utilizando zip y una cantidad arbitraria de componentes.
     Referencia: https://docs.python.org/3/tutorial/controlflow.html#unpacking-argument-lists
     """
-    pass # Completar
+    return tuple(zip(*args))
 
 
 # NO MODIFICAR - INICIO
