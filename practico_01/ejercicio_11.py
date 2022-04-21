1624a229b4fa194380f49c0837599a874f511ee7
MarcosLeones <87377508+MarcosLeones@users.noreply.github.com>
2022-04-21 17:56:36 -0300

    Update ejercicio_11.py

--- practico_01/ejercicio_11.py
+++ practico_01/ejercicio_11.py
@@ -34,7 +34,7 @@ def suma_cubo_pares_sum_list(numeros: Iterable[int]) -> int:
     Referencia: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
     Referencia: https://docs.python.org/3/library/functions.html#sum
     """
-    pass # Completar
+    return sum([n**3 for n in filter(lambda n: n%2==0, numeros)])
 
 
 # NO MODIFICAR - INICIO
@@ -50,7 +50,7 @@ def suma_cubo_pares_sum_gen(numeros: Iterable[int]) -> int:
     y la función sum.
     Referencia: https://docs.python.org/3/reference/expressions.html#generator-expressions
     """
-    pass # Completar
+    return sum(n**3 for n in filter(lambda n: n%2==0, numeros))
 
 
 # NO MODIFICAR - INICIO
@@ -70,25 +70,25 @@ numeros = [1, 2, 3, 4, 5, 6]
 
 # Escribir una función lambda que eleve los elementos al cubo
 
-numeros_al_cubo = # Completar
+numeros_al_cubo = list(map(lambda n: n**3, numeros))
 
 
 # Escribir una función lambda que permita filtrar todos los elementos pares
 
-numeros_al_cubo_pares = # Completar
+numeros_al_cubo_pares = list(filter(lambda n: n%2==0, numeros_al_cubo))
 
 
 # Escribir una función Lambda que sume todos los elementos
 
 from functools import reduce
 
-suma_numeros_al_cubo_pares = # Completar
+suma_numeros_al_cubo_pares = reduce(lambda n, m: n+m, numeros_al_cubo_pares)
 
 
 # Escribir una función Lambda que permita ordenar los elementos de la numeros
 # en base a si son pares o impares
 
-numeros_ordenada = # Completar
+numeros_ordenada = sorted(numeros, key=(lambda n: n%2), reverse=True)
 
 # NO MODIFICAR - INICIO
 assert numeros_al_cubo == [1, 8, 27, 64, 125, 216]
